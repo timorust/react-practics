372e91e8416067e8af76eee3a2babcbaf5b1d4ba
Timor Rust <timorust1@gmail.com>
2022-12-15 15:51:00 +0200

    class component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
-  const [likes, setLikes] = useState(5);
-  const [value, setValue] = useState("Input Text");
-
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function decrement() {
-    setLikes(likes - 1);
-  }
+  // const [value, setValue] = useState("Input Text");
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h2>{value}</h2>
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <ClassCounter />
     </div>
   );
 }
