bff462603ba526539af9b4e6094462dc0df99fd4
Timor Rust <timorust1@gmail.com>
2022-12-16 20:52:52 +0200

    get data from input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
-import ClassCounter from "./components/ClassCounter";
+import React, { useState } from "react";
+import PostsList from "./components/PostsList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import "./styles/App.css";
+// import ClassCounter from "./components/ClassCounter";
 
 function App() {
-  // const [value, setValue] = useState("Input Text");
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScrypt 1", body: "Description" },
+    { id: 2, title: "JavaScrypt 2", body: "Description" },
+    { id: 3, title: "JavaScrypt 3", body: "Description" },
+  ]);
+
+  const [title, setTitle] = useState("");
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+  };
+
   return (
     <div className="App">
-      <ClassCounter />
+      <form>
+        <MyInput
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          type="text"
+          placeholder="Post name"
+        />
+        <MyInput type="text" placeholder="Post content" />
+        <MyButton onClick={addNewPost}>Create Post</MyButton>
+      </form>
+      <PostsList posts={posts} title="JS Posts" />
     </div>
   );
 }
